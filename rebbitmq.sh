02a936f640f8f55ac79078338c34ec0237be33d5
somayya <somayya.adusumalli98@gmail.com>
2025-10-02 17:53:42 +0530

    session-06

--- rebbitmq.sh
+++ rebbitmq.sh
@@ -11,7 +11,7 @@ VALIDATE $? "Installing RabbitMQ Server"
 systemctl enable rabbitmq-server &>>$LOG_FILE
 VALIDATE $? "Enabling RabbitMQ Server"
 systemctl start rabbitmq-server &>>$LOG_FILE
-VALIDATE $? "Starting RabbitMQ Server"
+VALIDATE $? "Starting RabbitMQ"
 rabbitmqctl add_user roboshop roboshop123 &>>$LOG_FILE
 rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
 VALIDATE $? "Setting up permissions"
