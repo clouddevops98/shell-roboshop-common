fc0a06b734f7a856e2ba0d4740a43119531dbc33
somayya <somayya.adusumalli98@gmail.com>
2025-10-03 11:57:15 +0530

    session-06

--- common.sh
+++ common.sh
@@ -12,7 +12,7 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
 START_TIME=$(date +%s)
 SCRIPT_DIR=$PWD
 MONGODB_HOST=mongodb.somayya.fun
-
+MYSQL_HOST=mysql.somayya.fun
 mkdir -p $LOGS_FOLDER
 echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
